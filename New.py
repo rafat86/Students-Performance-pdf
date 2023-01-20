68143bcb32c015c13faf9841f969e50f5b9e6512
rafat86 <115078051+rafat86@users.noreply.github.com>
2023-01-20 19:07:16 +0200

    5th commit

--- New.py
+++ New.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 class StudentData:
 
@@ -8,11 +8,11 @@ class StudentData:
         self.std_df = pd.read_excel(path)
 
         columns_names = self.std_df.iloc[:, 3:].columns.tolist()
-        self.rubric_elements = len(columns_names)
-        self.rubric = columns_names[0:self.rubric_elements - 1]
+        self.rubric_length = len(columns_names)
+        self.rubric = columns_names[0:self.rubric_length - 1]
         self.rubric_weights = []
         i = 3
-        while i < (self.rubric_elements - 1) + 3:
+        while i < (self.rubric_length - 1) + 3:
             self.rubric_weights.append(self.std_df.iloc[0, i])
             i = i + 1
 
@@ -32,3 +32,26 @@ class StudentData:
         new_df = new_df.set_index("Names")
         return list(new_df.loc[student_name])[2:]
 
+    def missed_act(self, student_name):
+        student_act = self.student_grades(student_name)
+        print("You Missed This Activity/Activities:")
+        for i in range(0, self.rubric_length-1):
+            if pd.isnull(student_act[i]) == True:
+                print("\n", self.rubric[i])
+
+    def bar_chart(self, student_name):
+        labels = self.std_df.iloc[:, 3:].columns.tolist()
+        x = np.arange(len(labels))
+        student_marks = self.student_grades(student_name)
+        rubric_weight = self.rubric_weights + [100]
+        width = 0.25
+        fig, ax = plt.subplots()
+        ax.bar(x, rubric_weight, width, label="Full Grade", color="red")
+        ax.bar(x, student_marks, width, label="Your Grade", color="blue")
+        ax.set_ylabel('Grades')
+        ax.set_title('Activities')
+        ax.set_xticks(x)
+        ax.set_xticklabels(labels, rotation='horizontal')
+        ax.legend()
+        fig.tight_layout()
+        plt.savefig('charts/'+student_name+'.png', dpi=300, bbox_inches='tight')
--- test.py
+++ test.py
@@ -9,3 +9,9 @@ std.pie_chart_wights()
 student_name = input("Enter a name for a student:")
 print(std.student_grades(student_name))
 
+print(std.get_rubric_weights())
+
+
+print(std.missed_act(student_name))
+
+std.bar_chart(student_name)
